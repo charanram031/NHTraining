b984313c4ec0a5aad44b78cbc2075cdf26673e3b
charanram031 <98389024+charanram031@users.noreply.github.com>
2022-02-07 16:01:59 +0530

    7th Feb Assignment

---
+++ Day11Assignments/Day11Project3/Day11Project3/Day11Project3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11Project3
+{
+    class Employee
+    {
+        public int Id
+        { 
+           
+            get
+            {
+                return Id;
+            }
+            set
+            {
+                Id = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+        public string Designation
+        {
+            get
+            {
+                return Designation;
+            }
+            set
+            {
+                Designation = "Automation";
+            }
+        }
+        public int Salary 
+        {
+            get
+            {
+                return Salary;
+            }
+            set
+            {
+                Salary = 50000;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.ReadLine();
+        }
+    }
+}
