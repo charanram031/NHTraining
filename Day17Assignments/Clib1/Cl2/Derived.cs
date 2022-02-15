459a1366cb2246c4e5bb385969141be84339adea
charanram031 <charanram031@gmail.com>
2022-02-15 15:45:36 +0530

    15 Feb Assignment

---
+++ Day17Assignments/Clib1/Cl2/Derived.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clib1;
+
+namespace Cl2
+{
+    //Author: Rc
+    public class Derived:BaseClass
+    {
+        /// <summary>
+        /// This method is to read values
+        /// </summary>
+        public void Read()
+        {
+            a = 4;
+            b = 3;
+            c = 6;
+            d= 5;
+            e = 9;
+        }
+    }
+
+    public class Other
+    {
+        /// <summary>
+        /// This method is to read values
+        /// </summary>
+        public void Read()
+        {
+            BaseClass cb = new BaseClass();
+            cb.a = 9;
+            cb.b = 10;
+            cb.c = 11;
+            cb.d = 12;
+            cb.e = 13;
+        }
+    }
+}
---
+++ Day17Assignments/Clib1/Clib1/BaseClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clib1
+{
+    //Author:Rc
+    /******Purpose:To show Access Modifiers*********/
+
+    public class BaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+        /// <summary>
+        /// This method is to Readvalues
+        /// </summary>
+        public void ReadValues()
+        {
+            a = 5;
+            b = 6;
+            c = 7;
+            d = 5;
+            e = 2;
+        }
+    }
+    public class DerivedClass : BaseClass
+    {
+        /// <summary>
+        /// This method is also to read values
+        /// </summary>
+         public void Read()
+         {
+             a = 5;
+             b = 6;
+             c = 7;
+             d = 5;
+             e = 2;
+         }
+    }
+    public class OtherClass1
+    {
+        /// <summary>
+        /// This method is to read values
+        /// </summary>
+        public void ReadData()
+        {
+            BaseClass obj = new BaseClass();
+            obj.a = 5;
+            obj.b = 6;
+            obj.c = 7;
+            obj.d = 8;
+            obj.e = 10;
+            }
+        }
+    }
+}
+
